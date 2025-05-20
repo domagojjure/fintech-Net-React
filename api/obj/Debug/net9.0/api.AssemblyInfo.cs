e2d5ba7737acddc69081927a1dbfc5ddc72f670e
domagojjure <domagoj.jure@gmail.com>
2025-05-20 11:07:40 +0200

    .

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf27ef5596bf3ef8e17a978b40e81906adbf6b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287bb976406acd93a4648910c0b064de0752e24b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
